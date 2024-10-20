b23a64b5081021b1732652f201791dc5963348a1
Beiryu <dinhnguyenkhanh210401@gmail.com>
2024-10-20 08:04:37 +0700

    feat: add select assignee component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,8 +1,21 @@
 'use client';
 
 import { Select } from '@radix-ui/themes';
+import axios from 'axios';
+import { User } from 'next-auth';
+import { useEffect, useState } from 'react';
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>('/api/users');
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <div>
       <Select.Root>
@@ -10,7 +23,11 @@ const AssigneeSelect = () => {
         <Select.Content>
           <Select.Group>
             <Select.Label>Suggestions</Select.Label>
-            <Select.Item value="1">Mosh</Select.Item>
+            {users.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
           </Select.Group>
         </Select.Content>
       </Select.Root>
