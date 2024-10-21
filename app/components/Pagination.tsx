30bf9f3f1e4b9b9552f27c2c61c0e0d52465190f
beiryu <dinhnguyenkhanh210401@gmail.com>
2024-10-21 16:50:10 +0700

    feat: implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
@@ -5,6 +7,7 @@ import {
   DoubleArrowRightIcon,
 } from '@radix-ui/react-icons';
 import { Button, Flex, Text } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface Props {
   itemCount: number;
@@ -12,25 +15,54 @@ interface Props {
   currentPage: number;
 }
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const pageCount = Math.ceil(itemCount / pageSize);
 
   if (pageCount <= 1) return null;
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    router.push(`?${params.toString()}`);
+  };
 
   return (
     <Flex align="center" gap="2">
       <Text size="2">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button variant="ghost" size="2" disabled={currentPage === 1}>
+      <Button
+        variant="ghost"
+        size="2"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button variant="ghost" size="2" disabled={currentPage === 1}>
+      <Button
+        variant="ghost"
+        size="2"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <ChevronLeftIcon />
       </Button>
-      <Button variant="ghost" size="2" disabled={currentPage === pageCount}>
+      <Button
+        variant="ghost"
+        size="2"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <ChevronRightIcon />
       </Button>
-      <Button variant="ghost" size="2" disabled={currentPage === pageCount}>
+      <Button
+        variant="ghost"
+        size="2"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Pagination from './components/Pagination';
 
-export default function Home() {
+export default function Home({ searchParams }: { searchParams: { page: string } }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Pagination itemCount={100} pageSize={10} currentPage={1} />
+      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
     </main>
   );
 }
