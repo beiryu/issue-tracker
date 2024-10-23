4cc95997cc72e700bf3838c5966a0ab3a63947a3
Beiryu <dinhnguyenkhanh210401@gmail.com>
2024-10-23 07:40:55 +0700

    feat: build issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import LatestIssues from './LatestIssues';
+import prisma from '@/prisma/client';
+import IssueSummary from './IssueSummary';
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: 'OPEN' } });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LatestIssues />
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
     </main>
   );
 }
